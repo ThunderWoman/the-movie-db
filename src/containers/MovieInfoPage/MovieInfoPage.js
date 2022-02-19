2f2426e6ab4e4b7db116f852e5f4de0794c2cbe2
ThunderWoman <yuliiasigva@gmail.com>
2022-02-19 11:27:41 +0200

    Control

--- src/containers/MovieInfoPage/MovieInfoPage.js
+++ src/containers/MovieInfoPage/MovieInfoPage.js
@@ -17,7 +17,7 @@ const MovieInfoPage = () => {
 
     const {poster_path, title, release_date, overview, vote_average, budget} = movie;
 
-    const navigate = useNavigate()
+    const navigate = useNavigate();
     const back = () => {
         navigate(-1);
     };
@@ -26,7 +26,7 @@ const MovieInfoPage = () => {
         <div>
             <button className={css.buttonBack} onClick={back}>Back</button>
             <div className={css.MovieInfoPage}>
-                <img src={`https://image.tmdb.org/t/p/w400${movie.poster_path}`} alt={movie.title}/>
+                <img src={`https://image.tmdb.org/t/p/w400${poster_path}`} alt={movie.title}/>
                 <div className={css.column}>
                     <h2>{title}</h2>
                     <div>release date: {release_date}</div>
